28391fe3c0cc57e9526a7d1d2ff713b4e28b6eae
Collin Browse <cbrowse13@gmail.com>
2019-06-19 07:35:13 -0600

    Initial Commit

---
+++ Buds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buds
+//
+//  Created by Collin Browse on 6/19/19.
+//  Copyright © 2019 Collin Browse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
