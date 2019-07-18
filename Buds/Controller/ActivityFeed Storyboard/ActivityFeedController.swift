9e8965fbf9f0cc04bc7a9431527b1421e028f8f5
Collin Browse <cbrowse13@gmail.com>
2019-07-17 20:44:42 -0600

    Refactored Controllers into Folders

---
+++ Buds/Controller/ActivityFeed Storyboard/ActivityFeedController.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityFeedController.swift
+//  Buds
+//
+//  Created by Collin Browse on 7/17/19.
+//  Copyright © 2019 Collin Browse. All rights reserved.
+//
+
+import UIKit
+
+class ActivityFeedController: UITableViewController {
+    
+    
+    
+    
+    
+}
