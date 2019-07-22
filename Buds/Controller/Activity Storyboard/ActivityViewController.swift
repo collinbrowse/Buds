7e80597e4b721b65b36561ad0d070c61ef960c27
Collin Browse <cbrowse13@gmail.com>
2019-07-22 16:28:19 -0600

    Closing #37. Rearranged order of items. Added dynamic loading of Profile Picture, Name, and Location

--- Buds/Controller/Activity Storyboard/ActivityViewController.swift
+++ Buds/Controller/Activity Storyboard/ActivityViewController.swift
@@ -10,10 +10,12 @@ import Foundation
 import FirebaseAuth
 import Firebase
 import UIKit
+import SVProgressHUD
 
 class ActivityViewController: UIViewController {
     
     // Hook Up Outlets
+    @IBOutlet weak var profilePictureImageView: UIImageView!
     @IBOutlet weak var currentNameTextView: UITextView!
     @IBOutlet weak var currentLocationTextView: UITextView!
     @IBOutlet weak var noteTextField: UITextField!
@@ -22,36 +24,86 @@ class ActivityViewController: UIViewController {
     @IBOutlet weak var locationPlaceholderTextView: UITextView!
     @IBOutlet weak var ratingPlaceholderTextView: UITextView!
     @IBOutlet weak var strainPlaceholderTextView: UITextView!
-    
     @IBOutlet var collectionOfTextViews: Array<UITextView>! // = [UIView]
-
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     var handle: AuthStateDidChangeListenerHandle?
     var user: User?
     var selectedDetail: String?
+    var ref: DatabaseReference?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        noteTextField.becomeFirstResponder()
-        
     }
+    
+    
     override func viewWillAppear(_ animated: Bool) {
+        SVProgressHUD.show()
         handle = Auth.auth().addStateDidChangeListener { auth, user in
             if let user = user {
                 self.user = user
+                self.ref = Database.database().reference()
+                self.profilePictureImageView.layer.cornerRadius = self.profilePictureImageView.frame.size.width / 2
+                self.profilePictureImageView.clipsToBounds = true
+                self.loadUser()
             } else {
                 // No User is signed in.
+                SVProgressHUD.dismiss()
             }
         }
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         if handle != nil {
             Auth.auth().removeStateDidChangeListener(handle!)
         }
     }
+    
+    func loadUser() {
+        if user?.email != nil {
+            ref!.child("users").child(user!.uid).observeSingleEvent(of: .value) { (snapshot) in
+                // Get the Profile Picture, Name, Location
+                if let dictionary = snapshot.value as? NSDictionary {
+                    self.currentNameTextView.text = dictionary["name"] as? String
+                    self.currentLocationTextView.text = dictionary["location"] as? String
+                    
+                    if let profilePictureURL = dictionary["profilePicture"] as? String {
+                        let firebaseStorageRef = Storage.storage().reference(forURL: profilePictureURL)
+                        firebaseStorageRef.getData(maxSize: 4*1024*1024, completion: { (data, error) in
+                            if let error = error {
+                                print("There was an error getting this data \(error.localizedDescription)")
+                                // Profile Picture already has a default value
+                                return
+                            }
+                            if let data = data {
+                                self.profilePictureImageView.image = UIImage(data: data)
+                                
+                                SVProgressHUD.dismiss()
+                            }
+                        })
+                    }
+                }
+            }
+        }
+        SVProgressHUD.dismiss()
+    }
+    
+    // Method to show a popup alert to the user if they are unable to register
+    func showAlert(success: Bool, alertMessage: String) {
+        var title: String
+        if success {
+            title = "Added Activity"
+        }
+        else {
+            title = "Unable to Add Activity"
+        }
+        let alert = UIAlertController(title: title, message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
+    
+    // Set up some information when a segue is called
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let nav = segue.destination as? SmokingActivityTableViewController {
             nav.delegate = self
@@ -71,19 +123,9 @@ class ActivityViewController: UIViewController {
                     destinationVC.dataToRetrieve = "strain"
                 }
             }
-//            else if segue.identifier == "goToLocation" {
-//                if let destinationVC = segue.destination as? SmokingActivityTableViewController {
-//                    destinationVC.dataToRetrieve = "goToLocation"
-//                }
-//            }
         }
         else if let nav = segue.destination as? NewActivityLocationController {
             nav.locationDelegate = self
-//            if segue.identifier == "goToLocation" {
-//                if let destinationVC = segue.destination as? NewActivityLocationController {
-//
-//                }
-//            }
         }
     }
     
@@ -118,19 +160,7 @@ class ActivityViewController: UIViewController {
             showAlert(success: didAddActivity, alertMessage: "Please Try Again")
         }
     }
-    // Method to show a popup alert to the user if they are unable to register
-    func showAlert(success: Bool, alertMessage: String) {
-        var title: String
-        if success {
-            title = "Added Activity"
-        }
-        else {
-            title = "Unable to Add Activity"
-        }
-        let alert = UIAlertController(title: title, message: alertMessage, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
+   
     
     @IBAction func smokingStylePressed(_ sender: Any) {
         performSegue(withIdentifier: "goToSmokingStyle", sender: Any?.self)
@@ -181,9 +211,6 @@ extension ActivityViewController: ActivityDetailsDelegate {
         else if detail == "strain" {
             strainPlaceholderTextView.text = value
         }
-//        else if detail == "location" {
-//            locationPlaceholderTextView.text = value
-//        }
     }
     
     
