4dc1743c41547eafdeb76879be0025e619661f17
Collin Browse <cbrowse13@gmail.com>
2019-11-13 18:02:45 -0700

    Added Category of the effect when selecting effect in new Activity

--- Buds/Controller/Activity Tab/NewActivityTableViewController.swift
+++ Buds/Controller/Activity Tab/NewActivityTableViewController.swift
@@ -15,6 +15,7 @@ class NewActivityTableViewController: UITableViewController {
     
     var ref: DatabaseReference!
     var detailsListArray: [String] = []
+    var effectsDict = [String: String]()
     var dataToRetrieve: String?
     var delegate: ActivityDetailsDelegate?
     
@@ -40,12 +41,11 @@ class NewActivityTableViewController: UITableViewController {
                             self.detailsListArray.append(child.key)
                         } else if self.dataToRetrieve == "effects" {
                             self.detailsListArray.append(child.key)
+                            self.effectsDict[child.key] = child.value as? String
                         }
                         else {
                             self.detailsListArray.append(child.value as! String)
                         }
-                        
-                        
                     }
                     self.tableView.reloadData()
                 } else {
@@ -77,6 +77,7 @@ class NewActivityTableViewController: UITableViewController {
         // Configure the cell...
         if detailsListArray.count > 0 {
             cell.textLabel?.text = "\(detailsListArray[indexPath.row])"
+            cell.detailTextLabel?.text = effectsDict[detailsListArray[indexPath.row]]
         }
 
         return cell
@@ -89,51 +90,6 @@ class NewActivityTableViewController: UITableViewController {
         
         
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
 }
 
